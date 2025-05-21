d89feaab8120b7d3290ea0bc2afa83de62bec9c6
YuAhyeon <dkgus7123@naver.com>
2025-05-21 11:05:42 +0900

    chore: Airbnb ESLint 설정 및 테일윈드 config 구성

---
+++ tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+import typography from "@tailwindcss/typography";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [tailwindcssAnimate, typography],
+};
+
+export default config;
