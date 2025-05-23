1b3a87e51237b3b417ecc6a0a2d05620ec4a95f1
YuAhyeon <dkgus7123@naver.com>
2025-05-23 12:05:49 +0900

    feat: Top, Prev 아이콘 정의

---
+++ src/components/icon/Prev.tsx
@@ -0,0 +1,10 @@
+export default function Prev() {
+  return (
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M3.30837 12L13 22.0002L11.0619 24L0.401298 12.9999C0.144347 12.7347 0 12.375 0 12C0 11.625 0.144347 11.2653 0.401298 11.0001L11.0619 0L13 1.99976L3.30837 12Z"
+      fill="white"
+    />
+  );
+}
--- src/components/icon/index.ts
+++ src/components/icon/index.ts
@@ -1,2 +1,4 @@
 export { default as Light } from './Light';
 export { default as Dark } from './Dark';
+export { default as Prev } from './Prev';
+export { default as Top } from './Top';
