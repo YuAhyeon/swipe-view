50fb68040f3110fffc9be52b68d6351f0b229663
YuAhyeon <dkgus7123@naver.com>
2025-05-23 12:11:29 +0900

    feat: 콘텐츠 컴포넌트

---
+++ src/components/page/chart/ContentCuration/Content/index.tsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Content as CurationContent } from '@/types';
+
+export default function Content({ id, title, src }: CurationContent) {
+  return (
+    <Link href={`/content/${id}`} className="w-full flex items-start gap-[15px]">
+      <div className="relative w-[90px] h-[90px] rounded-lg overflow-hidden">
+        <Image
+          src={src}
+          alt="user profile"
+          fill
+          quality={75}
+          priority
+          placeholder="empty"
+          draggable="false"
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
+      <p className="flex-1 font-bold pt-[10px]">{title}</p>
+    </Link>
+  );
+}
