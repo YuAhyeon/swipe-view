96de9463832aa3823ec8e1af90210adacb179ac3
YuAhyeon <dkgus7123@naver.com>
2025-05-23 12:05:13 +0900

    feat: tanstack queryprovider 설정 및 theme 버튼 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import ThemeProvider from '@/components/themes/Theme-provider';
 import './globals.css';
+
+import Providers from '@/components/Providers';
+import ThemeProvider from '@/components/themes/Theme-provider';
+import { ThemeToggleButton } from '@/components/themes';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -25,15 +28,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main>{children}</main>
-        </ThemeProvider>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}>
+        <Providers>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main>{children}</main>
+            <ThemeToggleButton />
+          </ThemeProvider>
+        </Providers>
       </body>
     </html>
   );
